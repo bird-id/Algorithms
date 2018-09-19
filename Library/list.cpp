00dd3e4e790a7d38643ddd935e1cf32e4bfe00c7
Yana <30505186+bird-id@users.noreply.github.com>
2018-09-19 22:52:07 +0300

    Update list.cpp

--- Library/list.cpp
+++ Library/list.cpp
@@ -3,34 +3,46 @@
 
 struct ListItem
 {
+	int data;
+	ListItem *next;
 };
 
 struct List
 {
+	ListItem *first;
 };
 
 List *list_create()
 {
-    return NULL;
+	List *list = new List;// выделить память под корень списка
+	list->first = NULL;
+	return list; // вернуть адрес на корень списка
 }
 
 void list_delete(List *list)
 {
+	ListItem *el_ptr1 = list->first, *el_ptr2;
+	while (el_ptr1 != NULL) {
+		el_ptr2 = el_ptr1->next;
+		delete el_ptr1;
+		el_ptr1 = el_ptr2;
+	};
+	delete list;
 }
 
 ListItem *list_first(List *list)
 {
-    return NULL;
+    return list->first;
 }
 
 int list_item_data(ListItem *item)
 {
-    return 0;
+	return item->data;
 }
 
 ListItem *list_item_next(ListItem *item)
 {
-    return NULL;
+    return item->next;
 }
 
 ListItem *list_item_prev(ListItem *item)
@@ -40,17 +52,43 @@ ListItem *list_item_prev(ListItem *item)
 
 ListItem *list_insert(List *list, int data)
 {
-    return NULL;
+	ListItem *el = new ListItem;
+	el->data = data;
+	el->next = NULL;
+	if (list->first == NULL)
+		list->first=el;
+	else {
+		el->next = list->first;
+		list->first = el;
+	}
+	return el;
 }
 
 ListItem *list_insert_after(List *list, ListItem *item, int data)
 {
-    return NULL;
+	ListItem *el = new ListItem;
+	el->data = data;
+	el->next = item->next;
+	item->next = el;
+	return el;
 }
 
 ListItem *list_erase(List *list, ListItem *item)
 {
-    return NULL;
+	if (list->first == NULL) return NULL;
+	if (list->first == item) {
+		list->first = item->next;
+		delete item;
+		return list->first;
+	}
+	else {
+		ListItem *el_ptr = list->first;
+		while (el_ptr->next != item)
+			el_ptr = el_ptr->next;
+		el_ptr->next = item->next;
+		delete item;
+		return el_ptr;
+	}
 }
 
 ListItem *list_erase_next(List *list, ListItem *item)
